7edb00268dbece4b89379d6ef968d94363ddcbf2
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-02-24 16:19:38 -0500

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App container-lg text-light border">
       <NavBar />
+      <Routes>
+         <Route exact path="/" element={<Home />}/>
+      </Routes>
     </div>
   );
 }
