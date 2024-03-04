e91f800fd5dfa48154fe407e975d6b352e21364f
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-03-04 00:13:54 -0500

    about styling and second section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,12 +5,25 @@ import { aboutText1, aboutText2 } from './utilityAbout'
 function About() {
     
     return (
+        <div className='container-xl'>
+            <p className='display-4 text-start mt-2 m-lg-4 m-0'>About Us</p>
             <div id='about-wrapper' className='mt-3 d-flex flex-wrap align-items-center justify-content-center'>
-                <img src='../images/about/j&j1-crop-sq.jpg' alt='Jen and Jake' className='about-image-1'/>
+                <div className='about-image-1'>
+                    <img src='../images/about/j&j1-crop-sq.jpg' alt='Jen and Jake' className='w-100 rounded'/>
+                </div>
                 <div className='about-text fs-5'>
-                    <p>{aboutText1}</p>
+                    <p className='px-3'>{aboutText1}</p>
                 </div>
             </div>
+            <div id='about-wrapper' className='mt-3 d-flex flex-wrap align-items-center justify-content-center'>
+                <div className='about-text fs-5'>
+                    <p className='px-3'>{aboutText2}</p>
+                </div>
+                <div className='about-image-1'>
+                    <img src='../images/about/j&j1-crop-sq.jpg' alt='Jen and Jake' className='w-100 rounded'/>
+                </div>
+            </div>
+        </div>
     )
 }
 
