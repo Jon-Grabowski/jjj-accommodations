3034d0476dc86be0087a2a6d5b63f19f7a46461a
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-03-04 00:27:37 -0500

    more about styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,15 +9,15 @@ function About() {
             <p className='display-4 text-start mt-2 m-lg-4 m-0'>About Us</p>
             <div id='about-wrapper' className='mt-3 d-flex flex-wrap align-items-center justify-content-center'>
                 <div className='about-image-1'>
-                    <img src='../images/about/j&j1-crop-sq.jpg' alt='Jen and Jake' className='w-100 rounded'/>
+                    <img src='../images/about/j&j1-crop-sq.jpg' alt='Jen and Jake' className='image w-100 rounded'/>
                 </div>
-                <div className='about-text fs-5'>
-                    <p className='px-3'>{aboutText1}</p>
+                <div className='about-text fs-5 position-relative'>
+                    <p className='p-3'>{aboutText1}</p>
                 </div>
             </div>
             <div id='about-wrapper' className='mt-3 d-flex flex-wrap align-items-center justify-content-center'>
                 <div className='about-text fs-5'>
-                    <p className='px-3'>{aboutText2}</p>
+                    <p className='p-3'>{aboutText2}</p>
                 </div>
                 <div className='about-image-1'>
                     <img src='../images/about/j&j1-crop-sq.jpg' alt='Jen and Jake' className='w-100 rounded'/>
