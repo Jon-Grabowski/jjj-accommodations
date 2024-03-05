bf5bee42e358710f385eb10468110e41ed0b02c1
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-03-05 15:39:31 -0500

    noodling with carousel

--- src/components/Home/RentalSection.js
+++ src/components/Home/RentalSection.js
@@ -4,20 +4,23 @@ import { images } from './utilitiesHome';
 import './rentalsection.css'
 import { useNavigate } from 'react-router-dom';
 
+
+
 function RentalSection() {
     const navigate = useNavigate()
     function handleClick() {
         navigate('/rentals')
     }
 
+
     return (    
-        <div className='d-flex justify-content-center align-items-center'>
-            <div>
+        <div id='rental-section-wrapper' className='d-flex justify-content-center align-items-center'>
+            <div id='rentals-text'>
                 <h1>Rentals</h1>
                 <p>Check out our current listing of rental properties and plan your relaxing getaway in the beautiful Catskill Mountains!</p>
             </div>  
-            <div className='w-50'>
-                <ImageGallery items={images} autoPlay={true} slideInterval={5000} onClick={handleClick}/>
+            <div id='carousel-section' className='w-50'>
+                <ImageGallery items={images} autoPlay={true} slideInterval={5000} onClick={handleClick} showFullscreenButton={false} showThumbnails={false} showPlayButton={false} />
             </div>
         </div>  
     )
