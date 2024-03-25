e7e0ce5aacd045e4d61d78d37b72adcdb68d122d
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-03-25 13:36:14 -0400

    more layout adjustments

--- src/components/Rentals/Rentals.js
+++ src/components/Rentals/Rentals.js
@@ -11,6 +11,15 @@ function Rentals() {
         return <li>{amenity}</li>
     })
 
+    // const detailsList = details.map((detail) => {
+    //     return <li className='list-group-item'>{detail}</li>
+    // })
+
+    const detailsList = details.map((detail, index) => {
+        if (index === details.length-1) return <span className='ms-1 fst-italic'>{detail}</span>
+        else return <span className='ms-1 fst-italic'>{`${detail} •`}</span>
+    })
+
     return (
         <div className='mt-5 container-lg'>
             <h1>Rentals Page</h1>
@@ -22,36 +31,38 @@ function Rentals() {
                 <div className='col-md-5 d-flex flex-column' style={{height:'100%'}}>
                     <div className="mb-4 text-white">
                         <p className='fs-2 fw-bold mb-1'>{name}</p>
-                        <p className='fst-italic'>{location} - {type}</p>
+                        <p className='fst-italic mb-0'>{location} - {type}</p>
+                        <div className='d-flex justify-content-center' style={{fontSize: '14px'}}>
+                            {detailsList}
+                        </div>
                     </div>
                     <p className="card-text text-start">{description}</p>
+                    <div className=''>
+                        <h3 className='border-bottom pb-1 fs-3'>Amenities</h3>
+                        <ul className='text-start'>
+                            {amenitiesList}
+                        </ul>
+                    </div>
                 </div>
 
             </div>
 
-            <div className='row'>
-                <div className="col-md-7">
-                    <p>{details}</p>
-                </div>
-                <div className='col-md-5'>
-                    <h3 className='border-bottom pb-1'>Amenities</h3>
+            {/* <div className='row'>
+                <div className='col-md-6'>
+                    <h3 className='border-bottom pb-1 fs-3'>Amenities</h3>
                     <ul className='text-start'>
                         {amenitiesList}
                     </ul>
                 </div>
-            </div>
-
-            {/* <div className='d-flex justify-content-center flex-wrap bg-white bg-opacity-25'>
-                <div className=" border" style={{maxWidth: '20rem'}}>
-                    <p className="card-text">{description}</p>
-                </div>
-                <div className=' border'>
-                    <h3 className='border-bottom pb-1'>Amenities</h3>
-                    <ul className='text-start'>
-                        {amenitiesList}
-                    </ul>
-                </div>
+                <div className="col-md-6">
+                    <h3 className='border-bottom pb-1 fs-3'>Details</h3>
+                    <p>{detailsList}</p>
+                </div>  
             </div> */}
+
+            <div className='d-flex justify-content-center gap-3'>
+                
+            </div>
 
         </div>
     )
