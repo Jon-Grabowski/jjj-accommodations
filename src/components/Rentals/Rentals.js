8b4fddaa289155bf670de40dfd783a39f4428219
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-04-11 12:51:51 -0400

    created rental management top section

--- src/components/Rentals/Rentals.js
+++ src/components/Rentals/Rentals.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import RentalTopSection from './RentalTopSection';
 import FeaturedRental from './FeaturedRental';
 import { rentalProperties } from './utilitiesRental';
 import RentalCard from './RentalCard';
@@ -34,32 +35,35 @@ function Rentals() {
     })
 
     return (
-        <div className='mt-3 container-lg'>
-            <div className='feature-title rounded my-5' style={{backgroundImage: `url(${featureRental.images[0]})`}}>
-                <p className='display-5 fw-bold bg-black bg-opacity-50 pt-3 mb-0 rounded-top border-top' style={{fontStyle: 'oblique'}}>{featureRental.name}</p>
-                <div className="mb-4 text-white  bg-black bg-opacity-50 pb-4 rounded-bottom border-bottom">
-                    <p className='fst-italic fs-5 mb-0'>{location} - {type}</p>
-                    <div className='d-flex justify-content-center' style={{fontSize: '16px'}}>
-                        {detailsList}
+        <div>
+            <RentalTopSection />
+            <div className='mt-3 container-lg'>
+                <div className='feature-title rounded my-5' style={{backgroundImage: `url(${featureRental.images[0]})`}}>
+                    <p className='display-5 fw-bold bg-black bg-opacity-50 pt-3 mb-0 rounded-top border-top' style={{fontStyle: 'oblique'}}>{featureRental.name}</p>
+                    <div className="mb-4 text-white  bg-black bg-opacity-50 pb-4 rounded-bottom border-bottom">
+                        <p className='fst-italic fs-5 mb-0'>{location} - {type}</p>
+                        <div className='d-flex justify-content-center' style={{fontSize: '16px'}}>
+                            {detailsList}
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='row align-items-center'>
-                <FeaturedRental rental={featureRental} />
-            </div>
-
-            <div>
-                <h3 className='text-start mt-5'>Explore Our Properties:</h3>
-                <div id='rental-cards-wrapper'className='w-full d-flex gap-2 overflow-auto my-3'>
-                    {rentalCards}
+                <div className='row align-items-center'>
+                    <FeaturedRental rental={featureRental} />
                 </div>
-            </div>
 
-            <div className='d-flex justify-content-center gap-3'>
-                
-            </div>
+                <div>
+                    <h3 className='text-start mt-5'>Explore Our Properties:</h3>
+                    <div id='rental-cards-wrapper'className='w-full d-flex gap-2 overflow-auto my-3'>
+                        {rentalCards}
+                    </div>
+                </div>
 
+                <div className='d-flex justify-content-center gap-3'>
+                    
+                </div>
+
+            </div>
         </div>
     )
 }
