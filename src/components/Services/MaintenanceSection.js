5db566013c286c2bd2fb00c48a99b5da7d17a6eb
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-04-19 17:37:38 -0400

    created maintenance service cards

--- src/components/Services/MaintenanceSection.js
+++ src/components/Services/MaintenanceSection.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import MaintenanceCard from './MaintenanceCard'
+import { maintenanceCardData } from './utilitiesServices'
+
 
 function MaintenanceSection() {
+    const maintenanceCards = maintenanceCardData.map(service => {
+        return <MaintenanceCard key={service.title} service={service}/>
+    })
+
     return (
         <div>
             <div id='maintenance-services-banner'>
@@ -8,6 +15,9 @@ function MaintenanceSection() {
             </div>
             <div className='container-lg my-5 py-5'>
                 <h4 className='display-6 w-75 m-auto fst-italic'>"<span className='fw-bold display-5'>Keeping Your Property in Prime Condition:</span> Let Us Handle Maintenance or Connect You with Trusted Contractors for Hassle-Free Solutions!"</h4>
+                <div className='my-5 py-5 d-flex flex-wrap justify-content-evenly align-items-center'>
+                    {maintenanceCards}
+                </div>
             </div>
         </div>
     )
---
+++ src/components/Services/utilitiesServices.js
@@ -0,0 +1,26 @@
+const maintenanceCardData = [
+    {
+        title: 'Handyman',
+        body:"Tackle those pesky household tasks with our Handyman Services, covering a range of smaller jobs to keep your property in top shape. Whether it's fixing a leaky faucet, repairing a squeaky door, or installing shelves, our skilled handyman is at your service!",
+        details: {
+            'Handyman Service': '$50/hour or quote per job',
+            'House Calls': '$50 stop out fee plus additional hourly after 30 minutes',
+            'Garbage Removal': '$5 per bag (plus house call if not already on site)',
+            'Concierge Services': 'Available upon request, rates varies.'
+        },
+        img: '../images/services/maintenance-stock-2.jpg'
+    },
+    {
+        title: 'Contractors',
+        body:"When it comes to larger projects requiring specialized skills and equipment, count on us to coordinate with trusted contractors from our network of professionals. We'll take care of all communications, ensuring seamless coordination and clear expectations!",
+        details: {
+            'Network': 'Carpenters, Plumbers, HVAC, Electricians, and much more',
+            'Trust': 'We use contractors that we can vouch for',
+            'Communication': 'We\'ll be your point of contact for all work',
+            'Coordination':'Service scheduling and access to property',
+        },
+        img: '../images/services/maintenance-stock-3.jpg'
+    }
+]
+
+export {maintenanceCardData}
