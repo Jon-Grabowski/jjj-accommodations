b7dec0988c09f4503567efda5ff28203338fad15
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-04-29 13:48:24 -0400

    contact info layout adjustment

--- src/components/Contact/ContactInfo.js
+++ src/components/Contact/ContactInfo.js
@@ -5,14 +5,17 @@ import './contactinfo.css'
 function ContactInfo() {
     return (
         <div className='d-flex flex-column gap-5 fs-5 text-start mb-5'>
-            <div className='d-flex flex-wrap justify-content-start align-items-center' id='email-contact' onClick={() => window.location = 'mailto:jjjaccommodations@gmail.com'}>
-                <Mail className='me-3' size={48}/><span className='m-auto'>JJJAccommodations@gmail.com</span>
+            <div className='d-flex flex-column flex-wrap justify-content-start align-items-start' id='email-contact' onClick={() => window.location = 'mailto:jjjaccommodations@gmail.com'}>
+                <div className='d-flex align-items-end fst-italic mb-2'><Mail className='me-3' size={48}/><span className='fs-3'>E-mail:</span></div>
+                <span className='m-auto fs-4'>JJJAccommodations@gmail.com</span>
             </div>
-            <div className='d-flex flex-wrap justify-content-start align-items-center'>
-                <PhoneCall className='me-3' size={48}/><div className='d-flex flex-column'><span>+1 (845) 807-1601</span><span>+1 (845) 701-1087</span></div>
+            <div className='d-flex flex-wrap flex-column justify-content-start align-items-start'>
+                <div className='d-flex align-items-end fst-italic mb-2'><PhoneCall className='me-3' size={48}/><span className='fs-3'>Phone:</span></div>
+                <div className='d-flex flex-column'><span>Main: +1 (845) 807-1601</span><span>Alt: +1 (845) 701-1087</span></div>
             </div>
-            <div className='d-flex flex-wrap justify-content-start align-items-center'>
-                <Mailbox className='me-3' size={48}/><div className='d-flex flex-column'><span>PO Box 357</span><span>Mongaup Valley, NY 12762</span></div>
+            <div className='d-flex flex-wrap flex-column justify-content-start align-items-start'>
+                <div className='d-flex align-items-end fst-italic mb-2'><Mailbox className='me-3' size={48}/><span className='fs-3'>Address:</span></div>
+                <div className='d-flex flex-column'><span>PO Box 357</span><span>Mongaup Valley, NY 12762</span></div>
             </div>
         </div>
     )
--- src/components/Contact/ContactTopSection.js
+++ src/components/Contact/ContactTopSection.js
@@ -3,11 +3,11 @@ import './contacttopsection.css'
 
 function ContactTopSection() {
     return (
-        <div id='contact-top-section-wrapper' className='border-bottom'>
+        <div id='contact-top-section-wrapper'>
             <div className='bg-black bg-opacity-50 py-2'>
                 <div className=' m-auto'>
                     <div className='py-4'>
-                        <h1 className='d-inline display-3 border-bottom fw-bold px-4'>Contact Us</h1>
+                        <h1 className='d-inline display-3 border-bottom fw-bold fst-italic px-4'>Contact Us</h1>
                         {/* <p className='text-wrap fs-5 m-auto py-2' style={{maxWidth:'30rem'}}>Whether you're looking for Short Term Rental Management or Residential Property Management, we've got you covered!</p> */}
                         <p className='fs-6 fst-italic pt-3' >We'd love to hear from you!</p>
                     </div>
