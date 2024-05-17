1ec82c80895cf1c173b2665869f8bf59a71ed34d
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-05-17 12:55:56 -0400

    added animations to About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './about.css'
 import TopSectionAbout from './TopSectionAbout'
+import Animation from '../Utilities/Animation'
 import { aboutText1, aboutText2 } from './utilityAbout'
 
 function About() {
@@ -8,23 +9,33 @@ function About() {
     return (
         <div className=''>
             <TopSectionAbout />
-            <div className='about-wrapper'>
-                <p className='display-4 container-xl fst-italic mb-5 pb-5' style={{maxWidth: '60rem'}}>"<span className='fw-bold'>A family-run team</span> passionate about taking care of your property needs with a personal touch!"</p>
+            <div className='about-wrapper overflow-hidden'>
+                <Animation variant='slideLeft' duration={1.5}>
+                    <p className='display-4 container-xl fst-italic mb-5 pb-5' style={{maxWidth: '60rem'}}>"<span className='fw-bold'>A family-run team</span> passionate about taking care of your property needs with a personal touch!"</p>
+                </Animation>
                 <div className='d-flex flex-wrap align-items-center justify-content-center gap-1 gap-lg-5 bg-white bg-opacity-25'>
-                    <div className='about-image-1 mb-1 mb-lg-0'>
-                        <img src='../images/about/j&j1-crop-sq.jpg' alt='Jen and Jake' className='image w-100 rounded'/>
-                    </div>
-                    <div className='about-text fs-4'>
-                        <p className='p-3'>{aboutText1}</p>
-                    </div>
+                    <Animation variant='fadeIn' duration={1.5}>
+                        <div className='about-image-1 mb-1 mb-lg-0'>
+                            <img src='../images/about/j&j1-crop-sq.jpg' alt='Jen and Jake' className='image w-100 rounded'/>
+                        </div>
+                    </Animation>
+                    <Animation variant='slideLeft' duration={1.5}>
+                        <div className='about-text fs-4'>
+                            <p className='p-3'>{aboutText1}</p>
+                        </div>
+                    </Animation>
                 </div>
                 <div className='mt-5 d-flex flex-wrap align-items-center justify-content-center gap-1 gap-md-5'>
-                    <div className='about-text fs-4'>
-                        <p className='p-3'>{aboutText2}</p>
-                    </div>
-                    <div className='about-image-1'>
-                        <img src='../images/about/j&j1-crop-sq.jpg' alt='Jen and Jake' className='w-100 rounded'/>
-                    </div>
+                    <Animation variant='slideRight' duration={1.5}>
+                        <div className='about-text fs-4'>
+                            <p className='p-3'>{aboutText2}</p>
+                        </div>
+                    </Animation>
+                    <Animation variant='fadeIn' duration={1.5}>
+                        <div className='about-image-1'>
+                            <img src='../images/about/j&j1-crop-sq.jpg' alt='Jen and Jake' className='w-100 rounded'/>
+                        </div>
+                    </Animation>
                 </div>
             </div>
         </div>
