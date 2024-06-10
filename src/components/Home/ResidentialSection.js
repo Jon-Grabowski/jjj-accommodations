2058b9c6d418628f16a8a492d9947e751e4ff5b7
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-06-10 17:12:19 -0400

    residental section layout adjustments

--- src/components/Home/ResidentialSection.js
+++ src/components/Home/ResidentialSection.js
@@ -5,21 +5,28 @@ import { ArrowRight } from 'lucide-react';
 import { serviceCardData } from './utilitiesHome';
 
 function ResidentialSection() {
-    const {btnText, headline, body, image, link} = serviceCardData[0]
+    const {headline, body, image, link} = serviceCardData[0]
     return (
         <div className='bg-white bg-opacity-25'>
-            <div className='mb-4 py-4 px-2 bg-black bg-opacity-25 overflow-hidden'>
+            <div className='mb-4 py-4 px-2 bg-black bg-opacity-50 overflow-hidden'>
                 <Animation variant="slideLeft" duration={1.5}>
                     <h1 className='display-2 fw-bold fst-italic'>{headline}</h1>
                 </Animation>
             </div>
             <div className='container-lg'>
                 <div className='row pb-4'>
-                    <div className='col-md-6'>
+                    <div className='col-lg-6'>
                         <img src={image} alt='cleaning' className='rounded w-100'  />
                     </div>
-                    <div className='col-md-6 d-flex flex-column justify-content-center'>
-                        <span className='fs-4 mb-5'>{body}</span>
+                    <div className='col-lg-6 d-flex flex-column justify-content-center ps-4'>
+                        <h2 className='text-start mb-4'>Keep your home pristine and well-maintained with our comprehensive residential services:</h2>
+                        <ul className='text-start fs-3 mb-4'>
+                            <li>Cleaning Packages</li>
+                            <li>Handyman Services</li>
+                            <li>Contractor Connections</li>
+                            <li>Seasonal Services</li>
+                        </ul>
+                        {/* <span className='fs-4 mb-5'>{body}</span> */}
                         <Link to={link}><button className='btn-bg-blue px-4 py-2 border fw-bold text-light rounded-pill fs-5'>Learn More <ArrowRight/></button></Link>
                     </div>
                 </div>
