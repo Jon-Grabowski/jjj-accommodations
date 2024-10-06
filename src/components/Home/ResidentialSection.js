63e4a0bcd0ee1f830b83026939778931b7ae3a71
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-10-06 14:04:56 -0400

    reformat residential section

--- src/components/Home/ResidentialSection.js
+++ src/components/Home/ResidentialSection.js
@@ -24,12 +24,25 @@ function ResidentialSection() {
                         </div>
                     </Animation>
                 </div>
-                <div className='container-lg mt-5'>
-                    <div className='row pb-4'>
-                        <div className='col-lg-6'>
+                <div className='container-xxl d-flex row m-auto align-items-center mt-4 pt-2 pb-5'>
+                        <div className='col-xl-6 m-4 mx-auto'>
                             <img src='../images/services/services-card-image-1.jpg' alt='cleaning' className='rounded w-100'  />
                         </div>
-                        <div className='col-lg-6 d-flex flex-column justify-content-center ps-4'>
+                        <div className='col-xl-6 d-flex'>
+                                <div id='rentals-text' className='mx-auto mb-3 mb-lg-0 d-flex flex-column justify-content-between align-items-center'>
+                                    <div className='mb-3'>
+                                        <h2 className='text-start fs-3 mb-4' style={{"maxWidth":"43rem"}}>Keep your home pristine and well-maintained with our comprehensive residential services:</h2>
+                                        <ul className='text-start fs-4 mb-4'>
+                                            <li>Cleaning Packages</li>
+                                            <li>Handyman Services</li>
+                                            <li>Contractor Connections</li>
+                                            <li>Seasonal Services</li>
+                                        </ul>
+                                    </div>
+                                    <Link to='/residential'><button className='btn-bg-blue px-4 py-2 border fw-bold text-light rounded-pill fs-4'>Learn More <ArrowRight size={35}/></button></Link>
+                                </div>  
+                            </div>
+                        {/* <div className='m-auto col-lg-6 d-flex flex-column justify-content-center ps-4'>
                             <h2 className='text-start mb-4'>Keep your home pristine and well-maintained with our comprehensive residential services including:</h2>
                             <ul className='text-start fs-3 mb-5'>
                                 <li>Cleaning Packages</li>
@@ -38,8 +51,7 @@ function ResidentialSection() {
                                 <li>Seasonal Services</li>
                             </ul>
                             <Link to='/residential'><button className='btn-bg-blue px-4 py-2 border fw-bold text-light rounded-pill fs-4'>Learn More <ArrowRight size={35}/></button></Link>
-                        </div>
-                    </div>
+                        </div> */}
                 </div>
             </Animation>
         </div>
