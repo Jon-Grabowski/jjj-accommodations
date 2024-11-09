eadcddfab54fd2d67a8ec9c97ba133437586822a
Jonathan Grabowski <jon.grabowski@gmail.com>
2024-11-09 13:39:26 -0500

    reviews background color

--- src/components/Rentals/Rentals.js
+++ src/components/Rentals/Rentals.js
@@ -11,13 +11,15 @@ function Rentals() {
     return (
         <div className='overflow-hidden'>
             <RentalManagement />
-            <div className='container-xl d-flex flex-wrap justify-content-center align-items-center gap-5 my-5 py-5'>
-                <Animation variant='fadeIn' duration={2} delay={.5}>
-                    <img src='../images/services/namaste-sign.jpeg' alt='nameste' className='my-2' style={{width:'16rem'}}/>
-                </Animation>
-                <Animation variant='slideLeft' duration={1.2}>
-                    <ReviewCarousel />
-                </Animation>
+            <div className="black-banner">
+                <div className='container-xl d-flex flex-wrap justify-content-center align-items-center gap-5 my-5 py-5'>
+                    <Animation variant='fadeIn' duration={2} delay={.5}>
+                        <img src='../images/services/namaste-sign.jpeg' alt='nameste' className='my-2 rounded' style={{width:'16rem'}}/>
+                    </Animation>
+                    <Animation variant='slideLeft' duration={1.2}>
+                        <ReviewCarousel />
+                    </Animation>
+                </div>
             </div>
             <RentalListings />
         </div>
